b67b83a7187b40b25bf1287e65741698c9cc93c5
Airam Eliel <32375803+Luck07@users.noreply.github.com>
2024-08-15 16:30:04 -0300

    Ainda dando OverFlow
    Comentando o que tinha antes e coloquei o codigo do replit mas mesmo assim ainda nada

--- Camera/Captura de imagens/Bluetooth e HTML/EspCam/resize.ino
+++ Camera/Captura de imagens/Bluetooth e HTML/EspCam/resize.ino
@@ -3,25 +3,25 @@
 #include "esp_camera.h"
 
 // Definir os pinos da câmera para a ESP32-CAM
-#define PWDN_GPIO_NUM     32  
-#define RESET_GPIO_NUM    -1  
-#define XCLK_GPIO_NUM      0  
-#define SIOD_GPIO_NUM     26  
-#define SIOC_GPIO_NUM     27  
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
 
-#define Y9_GPIO_NUM       35  
-#define Y8_GPIO_NUM       34  
-#define Y7_GPIO_NUM       39  
-#define Y6_GPIO_NUM       36  
-#define Y5_GPIO_NUM       21  
-#define Y4_GPIO_NUM       19  
-#define Y3_GPIO_NUM       18  
-#define Y2_GPIO_NUM        5  
-#define VSYNC_GPIO_NUM    25  
-#define HREF_GPIO_NUM     23  
-#define PCLK_GPIO_NUM     22 
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
 
-#define LED_PIN 33 // Definir o pino do LED para a ESP32-CAM
+#define LED_PIN 33  // Definir o pino do LED para a ESP32-CAM
 
 // Definir um serviço BLE com UUID específico
 BLEService esp32Service("19B10000-E8F2-537E-4F6C-D104768A1213");
@@ -33,22 +33,19 @@ const int chunk = 100;
 BLECharacteristic enviarImagem("19B10000-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify, chunk);
 BLEUnsignedIntCharacteristic receberInt("19B10000-E8F2-537E-4F6C-D104768A1215", BLERead | BLEWrite);
 
-int data = 0; // Variável para armazenar dados recebidos
+int data = 0;  // Variável para armazenar dados recebidos
 
-#define size_linha 320
-#define size_coluna 240
-
-#define resize_linha 160
-#define resize_coluna 120
-
-
-
+#define TAM FRAMESIZE_SVGA
+#define o 120
+#define n 14400
+#define n_imagens 6
 
 void setup() {
-  Serial.begin(115200); 
-  while (!Serial); 
+  Serial.begin(115200);
+  while (!Serial)
+    ;
 
-  pinMode(LED_PIN, OUTPUT); 
+  pinMode(LED_PIN, OUTPUT);
 
   // Configurar as configurações da câmera
   camera_config_t config;
@@ -71,11 +68,11 @@ void setup() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   //config.xclk_freq_hz = 20000000; // Frequência do clock externo
-  config.xclk_freq_hz = 5000000; 
-  config.pixel_format = PIXFORMAT_GRAYSCALE; // Formato de pixel Grayscale para captura de imagem
-  config.frame_size = FRAMESIZE_QVGA; // Tamanho do frame SVGA (800x600)
-  config.jpeg_quality = 10; // Qualidade JPEG (0-63)
-  config.fb_count = 1; // Número de frame buffers
+  config.xclk_freq_hz = 5000000;
+  config.pixel_format = PIXFORMAT_GRAYSCALE;  // Formato de pixel Grayscale para captura de imagem
+  config.frame_size = FRAMESIZE_SVGA;                    // Tamanho do frame SVGA (Ver o TAM)
+  config.jpeg_quality = 10;                   // Qualidade JPEG (0-63)
+  config.fb_count = 1;                        // Número de frame buffers
 
   // Inicializar a câmera
   if (esp_camera_init(&config) != ESP_OK) {
@@ -91,7 +88,7 @@ void setup() {
 
   // Configurar BLE
   BLE.setLocalName("ESP32-CAM");
-  BLE.setAdvertisedService(esp32Service); 
+  BLE.setAdvertisedService(esp32Service);
   esp32Service.addCharacteristic(enviarImagem);
   esp32Service.addCharacteristic(receberInt);
 
@@ -105,15 +102,14 @@ void setup() {
 }
 
 void loop() {
-  BLEDevice central = BLE.central(); // Procurar por dispositivos BLE centrais conectados
+  BLEDevice central = BLE.central();  // Procurar por dispositivos BLE centrais conectados
 
-  if (central) { 
+  if (central) {
     Serial.print("Connected to central: ");
     Serial.println(central.address());
     digitalWrite(LED_PIN, LOW);
 
     while (central.connected()) {
-      
     }
 
     digitalWrite(LED_PIN, HIGH);
@@ -127,47 +123,77 @@ void dataWritten(BLEDevice central, BLECharacteristic characteristic) {
   uint8_t valor = receberInt.value();
   Serial.print("Valor Recebido: ");
   Serial.println(valor);
-  
-  if (valor == 1) { 
+
+  if (valor == 1) {
     Serial.println("Preparando foto");
-    camera_fb_t * fb = esp_camera_fb_get();
+    camera_fb_t* fb = esp_camera_fb_get();
     Serial.println("Foto tirada");
-    if (!fb) { 
+    if (!fb) {
       Serial.println("Falha ao capturar imagem!");
       return;
     }
 
-    int imageOffset = 0; 
-    uint8_t *img = fb->buf; 
-    uint8_t img_size[size_linha][size_coluna];
-    int teste = 0;
-    Serial.println("0");
-    for(int l = 0; l < size_linha; l++)
-      for (int c = 0; c < size_coluna; c++)
-      {
-        img_size[l][c] = img[teste];
-        teste++;
-      }
-      Serial.println("a");
-    teste = 0;
-    uint8_t img_enviar[resize_linha * resize_coluna];
-    for(int l = 0; l < resize_linha; l++)
-      for (int c = 0; c < resize_coluna; c++)
-      {
-        img_enviar[teste] = img_size[l][c];
-        teste++;
-      } 
-      Serial.println("c");
-    while (imageOffset < teste ) { 
-      int restante = teste - imageOffset; 
-      int bytesToSend = restante < chunk ? restante : chunk; 
-      enviarImagem.writeValue(&img_enviar[imageOffset], bytesToSend);
-      imageOffset += bytesToSend;
+    uint8_t* img = fb->buf;
+    int imageOffset = 0;
+    int seq[n_imagens] = { 0 };
+    uint8_t rec[n];
+    Serial.println("Teste (não passa daqui)");
+    int cont = 0;
+
+    for (int i = 0; i < TAM; i++) {
+      if (i % (n * o) == 0 || i % (n * o) == 2)
+        seq[cont++] = i;
     }
-    
-    Serial.print("Imagem enviada com sucesso! - ");
-    Serial.println(imageOffset);
-    
-    esp_camera_fb_return(fb);
+    Serial.println("Indo no for");
+    for (int k = 0; k < n_imagens; k++) {
+      int p = 0;
+      for (int q = seq[k]; p < n; q += n) {
+        // loop para separar uma imagem
+        for (int i = q; i < (q + o); i++) {
+          rec[p++] = img[i];
+        }
+        Serial.println("Enviando foto");
+      }
+      while (imageOffset < n) {
+        int restante = n - imageOffset;
+        int bytesToSend = restante < chunk ? restante : chunk;
+        enviarImagem.writeValue(&rec[imageOffset], bytesToSend);
+        imageOffset += bytesToSend;
+      }
+      Serial.println("Foto enviada");
+    }
+
+    // int imageOffset = 0;
+    // uint8_t *img = fb->buf;
+    // uint8_t img_size[size_linha][size_coluna];
+    // int ponteiro = 0;
+    // Serial.println("0");
+    // for(int l = 0; l < size_linha; l++)
+    //   for (int c = 0; c < size_coluna; c++)
+    //   {
+    //     img_size[l][c] = img[ponteiro];
+    //     ponteiro++;
+    //   }
+    //   Serial.println("a");
+    // ponteiro = 0;
+    // uint8_t img_enviar[resize_linha * resize_coluna];
+    // for(int l = 0; l < resize_linha; l++)
+    //   for (int c = 0; c < resize_coluna; c++)
+    //   {
+    //     img_enviar[ponteiro] = img_size[l][c];
+    //     ponteiro++;
+    //   }
+    //   Serial.println("c");
+    // while (imageOffset < ponteiro ) {
+    //   int restante = ponteiro - imageOffset;
+    //   int bytesToSend = restante < chunk ? restante : chunk;
+    //   enviarImagem.writeValue(&img_enviar[imageOffset], bytesToSend);
+    //   imageOffset += bytesToSend;
+    // }
+
+    // Serial.print("Imagem enviada com sucesso! - ");
+    // Serial.println(imageOffset);
+
+    // esp_camera_fb_return(fb);
   }
 }
