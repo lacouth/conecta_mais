08c0410fb71345679b2ad3df772fa4dea677030d
Airam Eliel <32375803+Luck07@users.noreply.github.com>
2024-10-23 17:59:30 -0300

    Colocando a arvore de decisao

---
+++ Redes/verificar_ambiente/4_modulo_light/DecisionTreeClassifier.h
@@ -0,0 +1,295 @@
+#pragma once
+#include <cstdarg>
+namespace Eloquent {
+    namespace ML {
+        namespace Port {
+            class DecisionTree {
+                public:
+                    /**
+                    * Predict class for features vector
+                    */
+                    int predict(float *x) {
+                        if (x[5] <= -54.5) {
+                            if (x[4] <= -62.5) {
+                                if (x[4] <= -82.5) {
+                                    if (x[1] <= -88.0) {
+                                        return 7;
+                                    }
+
+                                    else {
+                                        if (x[8] <= -42.5) {
+                                            return 4;
+                                        }
+
+                                        else {
+                                            return 3;
+                                        }
+                                    }
+                                }
+
+                                else {
+                                    if (x[37] <= -29.5) {
+                                        if (x[37] <= -74.5) {
+                                            if (x[5] <= -78.5) {
+                                                return 5;
+                                            }
+
+                                            else {
+                                                if (x[16] <= -46.5) {
+                                                    return 1;
+                                                }
+
+                                                else {
+                                                    if (x[2] <= -90.0) {
+                                                        return 5;
+                                                    }
+
+                                                    else {
+                                                        if (x[22] <= -79.0) {
+                                                            return 3;
+                                                        }
+
+                                                        else {
+                                                            return 4;
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+
+                                        else {
+                                            if (x[33] <= -43.0) {
+                                                return 4;
+                                            }
+
+                                            else {
+                                                return 2;
+                                            }
+                                        }
+                                    }
+
+                                    else {
+                                        if (x[5] <= -74.5) {
+                                            if (x[15] <= -42.0) {
+                                                if (x[47] <= -79.0) {
+                                                    if (x[4] <= -72.0) {
+                                                        if (x[47] <= -83.0) {
+                                                            return 4;
+                                                        }
+
+                                                        else {
+                                                            return 0;
+                                                        }
+                                                    }
+
+                                                    else {
+                                                        if (x[10] <= -91.5) {
+                                                            if (x[21] <= -90.5) {
+                                                                return 5;
+                                                            }
+
+                                                            else {
+                                                                return 0;
+                                                            }
+                                                        }
+
+                                                        else {
+                                                            return 0;
+                                                        }
+                                                    }
+                                                }
+
+                                                else {
+                                                    if (x[10] <= -86.5) {
+                                                        if (x[28] <= -45.0) {
+                                                            return 3;
+                                                        }
+
+                                                        else {
+                                                            return 8;
+                                                        }
+                                                    }
+
+                                                    else {
+                                                        if (x[21] <= -91.5) {
+                                                            return 5;
+                                                        }
+
+                                                        else {
+                                                            return 0;
+                                                        }
+                                                    }
+                                                }
+                                            }
+
+                                            else {
+                                                if (x[5] <= -80.0) {
+                                                    return 5;
+                                                }
+
+                                                else {
+                                                    if (x[10] <= -36.0) {
+                                                        return 0;
+                                                    }
+
+                                                    else {
+                                                        return 2;
+                                                    }
+                                                }
+                                            }
+                                        }
+
+                                        else {
+                                            if (x[23] <= -84.0) {
+                                                if (x[19] <= -43.0) {
+                                                    if (x[47] <= -34.0) {
+                                                        return 1;
+                                                    }
+
+                                                    else {
+                                                        return 3;
+                                                    }
+                                                }
+
+                                                else {
+                                                    if (x[2] <= -75.0) {
+                                                        return 4;
+                                                    }
+
+                                                    else {
+                                                        return 6;
+                                                    }
+                                                }
+                                            }
+
+                                            else {
+                                                if (x[5] <= -71.5) {
+                                                    if (x[40] <= -39.5) {
+                                                        return 5;
+                                                    }
+
+                                                    else {
+                                                        if (x[21] <= -91.0) {
+                                                            return 2;
+                                                        }
+
+                                                        else {
+                                                            return 4;
+                                                        }
+                                                    }
+                                                }
+
+                                                else {
+                                                    if (x[46] <= -43.0) {
+                                                        return 6;
+                                                    }
+
+                                                    else {
+                                                        return 7;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                            else {
+                                if (x[4] <= -56.5) {
+                                    if (x[46] <= -78.0) {
+                                        if (x[47] <= -82.5) {
+                                            return 0;
+                                        }
+
+                                        else {
+                                            if (x[15] <= -46.5) {
+                                                return 5;
+                                            }
+
+                                            else {
+                                                return 6;
+                                            }
+                                        }
+                                    }
+
+                                    else {
+                                        if (x[5] <= -78.5) {
+                                            return 8;
+                                        }
+
+                                        else {
+                                            if (x[10] <= -87.5) {
+                                                if (x[22] <= -48.5) {
+                                                    return 1;
+                                                }
+
+                                                else {
+                                                    return 8;
+                                                }
+                                            }
+
+                                            else {
+                                                return 1;
+                                            }
+                                        }
+                                    }
+                                }
+
+                                else {
+                                    if (x[2] <= -82.5) {
+                                        if (x[23] <= -94.0) {
+                                            return 1;
+                                        }
+
+                                        else {
+                                            return 8;
+                                        }
+                                    }
+
+                                    else {
+                                        if (x[19] <= -41.5) {
+                                            return 1;
+                                        }
+
+                                        else {
+                                            return 8;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        else {
+                            if (x[21] <= -39.0) {
+                                if (x[22] <= -82.5) {
+                                    return 7;
+                                }
+
+                                else {
+                                    if (x[21] <= -91.5) {
+                                        return 7;
+                                    }
+
+                                    else {
+                                        return 6;
+                                    }
+                                }
+                            }
+
+                            else {
+                                if (x[19] <= -40.5) {
+                                    return 7;
+                                }
+
+                                else {
+                                    return 2;
+                                }
+                            }
+                        }
+                    }
+
+                protected:
+                };
+            }
+        }
+    }
